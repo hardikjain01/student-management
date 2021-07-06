3daea78b5d173d1a7a93be927a940338baaafb35
Hardik Jain <hardikj476@gmail.com>
2021-07-06 10:07:32 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.student;
+
+//import java.time.LocalDate;
+//import java.time.Month;
+//import java.util.List;
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+	
+//	@Bean
+//	CommandLineRunner commandLineRunner(StudentRepository repository) {
+//		return args -> {
+//			Student hardik = new Student("Hardik", "hardikjain@gmail.com", LocalDate.of(1998, Month.DECEMBER, 30));
+//			Student sachin = new Student("Sachin", "sachingupta@gmail.com", LocalDate.of(1997, Month.AUGUST, 25));
+//			
+//			repository.saveAll(List.of(hardik, sachin));
+//		};
+//	}
+}
